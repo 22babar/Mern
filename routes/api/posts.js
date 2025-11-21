27cba943b32d6c28fc795ffd14206b45907da7e2
22babar <babar.pchs@gmail.com>
2025-11-21 15:43:56 +0500

    add some code in Profile

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,28 @@
 import express from 'express';
+import Profile from '../../Models/Profile.js';
+import auth from '../../middleware/auth.js';
 
-//@route    GET api/posts/
-//@desc     Test route
-//@access   Public
+//@route    GET api/posts/me
+//@desc     Get current users posts
+//@access   private
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Posts route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 export default router;
